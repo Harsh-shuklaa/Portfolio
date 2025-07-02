81c8bf46937e776da933af053ad85999cc779bd3
harsh <harsh9468121516@gmail.com>
2025-07-03 01:13:59 +0530

    Project add one

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <LeetCodeSection/>
         <Skills />
         <Experience />
-        {/* <Work /> */}
+        <Work />
         <Education />
         <Contact />
         <Footer />
